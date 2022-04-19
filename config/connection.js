594cc0345c4ebd2efe0ca7216dcb7f54b68fa912
Jared Haralson <jharalson29@gmail.com>
2022-04-19 14:32:19 -0400

    Application works locally. Changes made to CSS.

--- config/connection.js
+++ config/connection.js
@@ -7,13 +7,11 @@ if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
   sequelize = new Sequelize(
-  
-    'heroku_e9dd7ba12403b4f?reconnect=true',
-    'b9e3de41fc99fc',
-    '666069f3',
-
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
     {
-      host: 'us-cdbr-east-05.cleardb.net',
+      host: 'localhost',
       dialect: 'mysql',
       port: 3306
     }
