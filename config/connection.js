e61a5523faab84a7483c84badbf978c0c5119c3b
Jared Haralson <jharalson29@gmail.com>
2022-04-19 16:15:57 -0400

    Changing connection file

--- config/connection.js
+++ config/connection.js
@@ -4,8 +4,8 @@ require('dotenv').config();
 
 let sequelize;
 
-if (process.env.JAWSDB_URL) {
-  sequelize = new Sequelize(process.env.JAWSDB_URL);
+if (process.env.CLEARDB_DATABASE_URL) {
+  sequelize = new Sequelize(process.env.CLEARDB_DATABASE_URL);
 } else {
   sequelize = new Sequelize(
     'heroku_05f3259ad5fe8bd',
